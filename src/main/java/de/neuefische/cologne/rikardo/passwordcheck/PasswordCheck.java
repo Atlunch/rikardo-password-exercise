ea99de63b4ef7abfb756a9c4c16a37dca0e3eac9
Rikardo <rikardo@marenzzi.de>
2020-09-25 20:19:28 +0200

    add array input

--- src/main/java/de/neuefische/cologne/rikardo/passwordcheck/PasswordCheck.java
+++ src/main/java/de/neuefische/cologne/rikardo/passwordcheck/PasswordCheck.java
@@ -9,16 +9,38 @@ public class PasswordCheck {
     public static void main(String[] args) {
 
         Scanner input_value = new Scanner(System.in);
-        System.out.println("Bitte Password eingeben: ");
-        String password = input_value.nextLine();
 
-        if (checkPassword(password)) {
-            System.out.println("Password ist korrekt");
-        } else {
-            System.out.println("Password ist NICHT korrekt");
+        System.out.println("________________Passwort Validierungs Programm_______________________");
+        System.out.println("|  Vorgefertige Passwörter verwenden oder neue Passwörter anlegen?  |");
+        System.out.println("|  - [1] vorgefertige Passwörter validieren                         |");
+        System.out.println("|  - [2] neues Passwort anlegen und anschließend validieren        |");
+        System.out.println("_____________________________________________________________________");
+        System.out.println("Bitte Zahl eingeben und bestätigen: ");
+
+        int task = input_value.nextInt();
+
+        switch (task) {
+            case 1:
+                String[] passwords = {"afsjgks","aksjdscswd","adsacsa214sad2s","Asfe23sasdSW","asd23adASD?!a..!2s"};
+                System.out.println("Folgende Passwörte werde nun validiert" + " " + java.util.Arrays.toString(passwords));
+                for (String s : passwords) {
+                    System.out.println(s + " " + ":" + " " + checkPassword(s));
+                }
+                break;
+            case 2:
+                System.out.println("Passwort Regeln: mind. 10 Zeichen, a-z, A-Z, 0-9");
+                System.out.println("Bitte Password eingeben: ");
+                Scanner input_value2 = new Scanner(System.in);
+                String password = input_value2.nextLine();
+                System.out.println(checkPassword(password));
+                break;
+
         }
+    }
 
-
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 
     public static boolean checkPassword(String password) {
