83f3416c17f326f423c5648bb2717478a4f346b7
Rikardo <rikardo@marenzzi.de>
2020-09-25 15:38:55 +0200

    check Password length, number, loweruppercase and special chars

--- src/main/java/de/neuefische/cologne/rikardo/passwordcheck/PasswordCheck.java
+++ src/main/java/de/neuefische/cologne/rikardo/passwordcheck/PasswordCheck.java
@@ -1,4 +1,34 @@
 package de.neuefische.cologne.rikardo.passwordcheck;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordCheck {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+    public static boolean checkPasswordLength(String password) {
+        return password.length() >=  10;
+    }
+
+    public static boolean checkIfPasswordIncludeNumber(String password) {
+        return password.matches(".*\\d.*");
+    }
+
+    public static boolean checkIfPasswordIncludeLowerUppercase(String password) {
+        return !password.equals(password.toLowerCase()) && !password.equals(password.toUpperCase());
+    }
+
+    public static boolean checkPasswordSpecialChars(String password) {
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9]*");
+        Matcher matcher = pattern.matcher(password);
+        return !matcher.matches();
+    }
+
 }
--- src/test/java/de/neuefische/cologne/rikardo/passwordcheck/PasswordCheckTest.java
+++ src/test/java/de/neuefische/cologne/rikardo/passwordcheck/PasswordCheckTest.java
@@ -1,7 +1,82 @@
 package de.neuefische.cologne.rikardo.passwordcheck;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordCheckTest {
+
+    //Check Password Length
+    @Test
+    @DisplayName("Das Passwort enthält 10 Zeichen")
+    public void testcheckPasswordLengthTrue(){
+        String password = "aaaaaaaaaa";
+        boolean result = PasswordCheck.checkPasswordLength(password);
+        assertTrue(result);
+    }
+    @Test
+    @DisplayName("Das Passwort hat nicht 10 Zeichen")
+    public void testcheckPasswordLengthFalse(){
+        String password = "aaaaaaaaa";
+        boolean result = PasswordCheck.checkPasswordLength(password);
+        assertFalse(result);
+    }
+
+    //Check If Password Include Number
+    @Test
+    @DisplayName("Das Passwort enthält eine Zahl")
+    public void testcheckIfPasswordIncludeNumberTrue(){
+        String password = "aaaaaaaaa1";
+        boolean result = PasswordCheck.checkIfPasswordIncludeNumber(password);
+        assertTrue(result);
+    }
+    @Test
+    @DisplayName("Das Passwort enthält keine Zahl")
+    public void testcheckIfPasswordIncludeNumberFalse(){
+        String password = "aaaaaaaaaa";
+        boolean result = PasswordCheck.checkIfPasswordIncludeNumber(password);
+        assertFalse(result);
+    }
+
+    //Check If Password Include Lowercase and Uppercase
+    @Test
+    @DisplayName("Das Passwort muss sowohl große als auch kleine Buchstaben enthalten")
+    public void testcheckIfPasswordIncludeLowerUppercaseTrue(){
+        String password = "aaaaaaaaA";
+        boolean result = PasswordCheck.checkIfPasswordIncludeLowerUppercase(password);
+        assertTrue(result);
+    }
+    @Test
+    @DisplayName("Das Passwort enthält nur kleine Buchstaben")
+    public void testcheckIfPasswordIncludeOnlyLowercaseFalse(){
+        String password = "aaaaaaaaa";
+        boolean result = PasswordCheck.checkIfPasswordIncludeLowerUppercase(password);
+        assertFalse(result);
+    }
+    @Test
+    @DisplayName("Das Passwort enthält nur große Buchstaben")
+    public void testcheckIfPasswordIncludeLOnlyUppercaseFalse(){
+        String password = "AAAAAAAAAA";
+        boolean result = PasswordCheck.checkIfPasswordIncludeLowerUppercase(password);
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("Das Passwort enthält Sonderzeichen")
+    public void testcheckPasswordSpecialCharsTrue(){
+        String password = "AAAAAAAAA?!";
+        boolean result = PasswordCheck.checkPasswordSpecialChars(password);
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Das Passwort enthält keine Sonderzeichen")
+    public void testcheckPasswordSpecialCharsFalse(){
+        String password = "AAAAAAAAAAA";
+        boolean result = PasswordCheck.checkPasswordSpecialChars(password);
+        assertFalse(result);
+    }
+
 
 }
